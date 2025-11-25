ebc3bb52691c271a1c13df4c3120a649986d5ee5
Kyunghee Kim <kkh2415@naver.com>
2025-11-25 23:20:10 +0900

    add dataset

--- code/register.py
+++ code/register.py
@@ -4,7 +4,7 @@ import model
 import utils
 from pprint import pprint
 
-if world.dataset in ['gowalla', 'yelp2018', 'amazon-book', 'movie']:
+if world.dataset in ['gowalla', 'yelp2018', 'amazon-book', 'movie', 'amazon']:
     dataset = dataloader.Loader(path="../data/"+world.dataset)
 elif world.dataset == 'lastfm':
     dataset = dataloader.LastFM()

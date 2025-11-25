c28228dea600ec4232464173ce1457aef688f192
Kyunghee Kim <kkh2415@naver.com>
2025-11-25 23:56:25 +0900

    test metrix logging

--- code/main.py
+++ code/main.py
@@ -17,6 +17,17 @@ print(">>SEED:", world.seed)
 import register
 from register import dataset
 
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+    
+if not os.path.exists('embs'):
+    os.mkdir('embs')
+    
+log_template = f'{world.args.dataset}_seed{world.args.seed}_{world.args.model}_dim{world.args.recdim}_lr{world.args.lr}_dec{world.args.decay}'
+run_datetime = strftime("%Y%m%d-%H%M%S")
+file_prefix = f'{log_template}_{run_datetime}'
+log_path = f'logs/{file_prefix}.txt'
+
 Recmodel = register.MODELS[world.model_name](world.config, dataset)
 Recmodel = Recmodel.to(world.device)
 bpr = utils.BPRLoss(Recmodel, world.config)
@@ -45,7 +56,20 @@ try:
         start = time.time()
         if epoch % 5 == 0:
             cprint("[TEST]")
-            Procedure.Test(dataset, Recmodel, epoch, w, world.config['multicore'])
+            test_results = Procedure.Test(dataset, Recmodel, epoch, w, world.config['multicore'])
+            
+            if len(world.topks) == 2:
+                test_log = [test_results['ndcg'][0], test_results['ndcg'][1],
+                            test_results['recall'][0], test_results['recall'][1],
+                            test_results['precision'][0], test_results['precision'][1]]
+            elif len(world.topks) == 1:
+                test_log = [test_results['ndcg'][0],
+                            test_results['recall'][0],
+                            test_results['precision'][0]]
+            
+            with open(log_path, 'a') as f:
+                f.write(f'test ' + ' '.join([str(x) for x in test_log]) + '\n')
+                
         output_information = Procedure.BPR_train_original(dataset, Recmodel, bpr, epoch, neg_k=Neg_k,w=w)
         print(f'EPOCH[{epoch+1}/{world.TRAIN_epochs}] {output_information}')
         torch.save(Recmodel.state_dict(), weight_file)
